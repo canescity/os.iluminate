065b944cc9daf1930cacbc69eee6001bde7bbba1
canescity <msf.01@gmx.com>
2024-12-09 16:39:16 -0300

    Create os.iluminate.sh

---
+++ os.iluminate.sh
@@ -0,0 +1,48 @@
+GNU nano 8.2                           NetSexy.sh
+#!/system/xbin/bash
+
+clear
+cd module
+cat baner.txt | lolcat
+python3 acceso.py
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+
+
+
+read -p "MOVIMENTO NACIONAL : NAZA :" bro
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if [ $bro = NAZA ] || [ $bro = NAZA ]
+then
+figlet "NetSexy" | lolcat
+php cc-br.php
+fi
